50273b76e529ea550c37c9310f7aa1f07d52e587
sannanhafeez <sannanhafeez06@gmail.com>
2020-06-03 17:28:28 +1200

    Complete code

--- client/index.js
+++ client/index.js
@@ -1,1 +1,88 @@
-/** THIS IS WHERE WE WILL WRITE OUR CLIENT SIDE LOGIC */
+const recipeNameElement = document.getElementById("recipe-name");
+const recipeContentElement = document.getElementById('recipe-content');
+const submitRecipeElement = document.getElementById('submit-recipe');
+const refreshRecipesElement = document.getElementById('refresh-recipes');
+const recipeDivElement = document.getElementById('recipes-div');
+const deleteRecipeId = document.getElementById('recipe-id-to-delete');
+const deleteRecipeElement = document.getElementById('delete-recipe');
+
+submitRecipeElement.addEventListener("click", () => {
+    // Submit button is clicked
+    addNewRecipe(recipeNameElement.value, recipeContentElement.value);
+});
+
+refreshRecipesElement.addEventListener("click", () => {
+    // Refresh //
+    getAllRecipes();
+});
+
+deleteRecipeElement.addEventListener("click", () => {
+    // Delete //
+    deleteRecipe(deleteRecipeId.value);
+});
+
+async function deleteRecipe(recipeId) {
+    const response = await fetch(`http://localhost:3000/recipe/${recipeId}`, {
+        method: "DELETE"
+    });
+
+    if (response.status === 200) {
+        alert("Recipe deleted");
+    } else {
+        alert("Could not delete");
+    }
+}
+
+async function getAllRecipes() {
+    recipeDivElement.innerHTML = "";
+
+    const response = await fetch("http://localhost:3000/recipe", {
+        method: "GET"
+    });
+    const data = await response.json();
+    const recipes = data.recipes;
+
+    recipes.forEach(recipe => {
+        const recipeElement = () => {
+            const recipeElement = document.createElement("div");
+            const recipeId = document.createElement("h5");
+            const recipeName = document.createElement("h5");
+            const recipeContent = document.createElement("p");
+
+            recipeId.innerHTML = "ID: " + recipe.id;
+            recipeName.innerHTML = "Recipe name: " + recipe.name;
+            recipeContent.innerHTML = "Recipe content: " + recipe.content;
+
+            recipeElement.appendChild(recipeId);
+            recipeElement.appendChild(recipeName);
+            recipeElement.appendChild(recipeContent);
+            recipeElement.className = "recipe-element"
+
+            return recipeElement;
+        };
+        recipeDivElement.appendChild(recipeElement());
+    });
+}
+
+async function addNewRecipe(recipeName, recipeContent) {
+    const data = {
+        "name": recipeName,
+        "content": recipeContent
+    };
+
+    const response = await fetch("http://localhost:3000/recipe", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.status === 201) {
+        // Success
+        alert("Success!");
+    } else {
+        // Failure
+        alert("Could not add recipe!");
+    }
+}
--- server/app.js
+++ server/app.js
@@ -7,6 +7,79 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let recipes = [];
+/**
+ * Three properties:
+ *  - ID: integer
+ *  - Name
+ *  - Content
+ */
+
+// GET ALL THE RECIPES
+app.get("/recipe", (req, res) => {
+    const data = {
+        "recipes": recipes
+    };
+
+    res.status(200).send(data);
+});
+
+// GET SPECIFIC RECIPE BY ID
+app.get("/recipe/:id", (req, res) => {
+    const requestedId = parseInt(req.params.id);
+
+    const requestedRecipe = recipes.find(recipe => recipe.id === requestedId);
+
+    if (requestedRecipe === undefined) {
+        // Not found
+        res.status(404).send("Recipe not found");
+    } else {
+        // Found
+        const data = {
+            "recipe": requestedRecipe
+        };
+        res.status(200).send(data);
+    }
+});
+
+// ADDING A NEW RECIPE
+app.post("/recipe", (req, res) => {
+    let newId;
+
+    if (recipes.length > 0) {
+        // Recipes exist
+        newId = recipes[recipes.length - 1].id + 1;
+    } else {
+        // None yet exist
+        newId = 1
+    }
+
+    const newRecipe = {
+        "id": newId,
+        "name": req.body.name,
+        "content": req.body.content
+    };
+    console.log(newRecipe);
+
+    recipes.push(newRecipe);
+
+    res.status(201).send("Added new recipe");
+});
+
+// DELETE RECIPE BY ID
+app.delete("/recipe/:id", (req, res) => {
+    const requestedId = parseInt(req.params.id);
+
+    if (recipes.some(recipe => recipe.id === requestedId)) {
+        // Recipe exists
+        recipes = recipes.filter(recipe => recipe.id !== requestedId);
+        res.status(200).send("Recipe has been deleted");
+    } else {
+        // Doesn't exist
+        res.status(404).send("Recipe not found");
+    }
+});
+
 app.listen(3000, () => {
     console.log('API is running!');
 });
